812372704de2a64852f4faa8432005315f465a45
Eric-Catarina <2020954200@teiacoltec.org>
2022-07-27 17:36:10 -0300

    Minor Changes

--- src/App.js
+++ src/App.js
@@ -38,18 +38,18 @@ function App() {
 
   return (
     <>
-      <form className='row g-3 mt-3'>
+      <form className='row g-3 mt-3 '>
         <div className="row">
-          <div className="col-md-6">
+          <div className="col ">
             <label for="ID" className='form-label'>ID</label>
-            <input id="id" type="text" class="form-control" placeholder="id" aria-label="First name" />
+            <input id="id" type="text" class="form-control" placeholder="ID" aria-label="First name" />
           </div>
-          <div className="col-md-6">
+          <div className="col">
             <label for="Jogo Favorito" className='form-label'>Jogo Favorito</label>
             <input id="descricao" type="text" className="form-control" placeholder="jogo favorito" aria-label="Last name" />
           </div>
 
-          <hr></hr>
+          <hr className='mt-3'></hr>
           <div className="col-12">
             <button onClick={adicionaJogoNaLista} type='button' className='btn btn-primary'>Enviar</button>
           </div>
@@ -58,13 +58,18 @@ function App() {
 
 
 
-      <div className='mt-3'>
+      <div className='mt-3 '>
+
+        {jogos.map(jogoAtual => (
+    
        
-          {jogos.map(jogoAtual => (
-            <li key={jogoAtual.id} className='list-group-item'>{jogoAtual.id} -      Jogo Favorito: {jogoAtual.jogoFavorito}</li>
-          ))}
-          
-        
+        <div key={jogoAtual.id} className="card mb-2 shadow" >
+
+          <div className="card-body">
+            <p className="card-text">{jogoAtual.id} - Jogo Favorito: {jogoAtual.jogoFavorito}</p>
+          </div>
+        </div>
+       ))}
       </div>
     </>
   );
